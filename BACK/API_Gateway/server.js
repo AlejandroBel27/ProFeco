b63a351c3355a2c79205f1e4c912945218950348
linmoreno <linda.moreno216690@potros.itson.edu.mx>
2025-12-08 00:56:55 -0700

    setup

--- BACK/API_Gateway/server.js
+++ BACK/API_Gateway/server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const WebSocket = require('ws');
 const cors = require('cors');
 
-const app = express();
+const app = express(); 
 const port = 3000;
 
 // Configuración básica
